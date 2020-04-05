3b8dd39f34b542d3daf367d7833be655c8a85fb6
CPerez816 <Carlosocbjj@gmail.com>
2020-04-04 22:17:22 -0400

    this is the calendar homework due on 4/4/2020

---
+++ HW4420/Calendar.js
@@ -0,0 +1,22 @@
+$(document).ready
+
+var moment = require('moment');
+
+moment().format();
+
+import moment from 'moment';
+const timeMoment = moment(1540500979000);
+timeMoment.format('HH:mm:ss, YYYY-MM-DD');
+console.log(timeMoment);
+
+//var p = moment.min.js;
+
+//creating a new calendar instance
+//var calendar = new p.Calendar(document.getElementById("calendar"));
+
+
+
+//visualizing the calendar
+//calendar.render();
+
+//moment().format('MMMM Do YYYY, h:mm:ss a');
