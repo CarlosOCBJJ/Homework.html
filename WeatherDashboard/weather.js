ece04db00dd9656f3e55ece23ab364822a7522a3
CPerez816 <Carlosocbjj@gmail.com>
2020-04-11 21:45:19 -0400

    This is my homework due 4/11/2020

---
+++ WeatherDashboard/weather.js
@@ -0,0 +1,15 @@
+function buildQueryUrl(){
+    var queryURL = http:api.openweathermap.org/data/2.5/forecast?id=524901&APPID={20b8fc41c2548427df66d29c64f22ae6}
+
+
+function displayCityInfo(){var city=
+    $(this).attr("#citylist");
+    var queryUrl = "http:api.openweathermap.org/data/2.5/forecast?id=524901&APPID={20b8fc41c2548427df66d29c64f22ae6}" 
+
+  var queryParams = { "api-key": "20b8fc41c2548427df66d29c64f22ae6"};
+
+
+
+
+
+}
